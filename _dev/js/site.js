7bafa0e8f0ce17d3176357f0261a45a9b5dcd071
Jason Kühn <spaceyraygun@gmail.com>
2016-11-29 18:06:27 -0500

    test

--- _dev/js/site.js
+++ _dev/js/site.js
@@ -2,8 +2,4 @@ $(function(){
     $('nav a, .rsvp').smoothScroll({
         offset: -50
     });
-
-    setTimeout(function(){
-        $('.rsvpify').attr('src', 'rsvpify.html');
-    }, 2000);
 });
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,1 @@
-$(function(){$("nav a, .rsvp").smoothScroll({offset:-50}),setTimeout(function(){$(".rsvpify").attr("src","rsvpify.html")},2e3)});
+$(function(){$("nav a, .rsvp").smoothScroll({offset:-50})});
