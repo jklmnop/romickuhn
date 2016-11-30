7e38069e933ea83bac53e23658d99c76f413c68a
Jason Kühn <spaceyraygun@gmail.com>
2016-11-30 16:02:10 -0500

    undo last commit

--- _dev/js/site.js
+++ _dev/js/site.js
@@ -2,19 +2,4 @@ $(function(){
     $('nav a, .rsvp').smoothScroll({
         offset: -50
     });
-
-    $(window)
-        .on('scroll', function(){
-            $(window).trigger('rsvpify').off('rsvpify');
-        })
-        .on('rsvpify', function(){
-            $('.rsvpify')
-                .attr('src', 'rsvpify.html')
-                .on('ready', function(){
-                    $('.rsvpify').attr('src', 'rsvpify.html');
-                });
-        })
-        .on('message', function(event) {
-            $('.rsvpify').height(event.originalEvent.data);
-        });
 });
