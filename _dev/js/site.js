0e95a3bd701c96521424be32939e8b89243935d3
Jason Kühn <spaceyraygun@gmail.com>
2016-11-30 15:52:08 -0500

    try this

--- _dev/js/site.js
+++ _dev/js/site.js
@@ -2,4 +2,19 @@ $(function(){
     $('nav a, .rsvp').smoothScroll({
         offset: -50
     });
+
+    $(window)
+        .on('scroll', function(){
+            $(window).trigger('rsvpify').off('rsvpify');
+        })
+        .on('rsvpify', function(){
+            $('.rsvpify')
+                .attr('src', 'rsvpify.html')
+                .on('ready', function(){
+                    $('.rsvpify').attr('src', 'rsvpify.html');
+                });
+        })
+        .on('message', function(event) {
+            $('.rsvpify').height(event.originalEvent.data);
+        });
 });
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,1 @@
-$(function(){$("nav a, .rsvp").smoothScroll({offset:-50})});
+$(function(){$("nav a, .rsvp").smoothScroll({offset:-50}),$(window).on("scroll",function(){$(window).trigger("rsvpify").off("rsvpify")}).on("rsvpify",function(){$(".rsvpify").attr("src","rsvpify.html").on("ready",function(){$(".rsvpify").attr("src","rsvpify.html")})}).on("message",function(i){$(".rsvpify").height(i.originalEvent.data)})});
